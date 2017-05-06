0d062d82ac2864a2eba627478a620b22c9dc2f81
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2017-05-06 13:33:00 +0300

    update packages

--- elpa/js3-mode-20160515.850/js3-mode-autoloads.el
+++
@@ -1,43 +0,0 @@
-;;; js3-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "js3" "js3.el" (22369 32101 82523 776000))
-;;; Generated autoloads from js3.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "js3-mode" "js3-mode.el" (22369 32101 57523
-;;;;;;  770000))
-;;; Generated autoloads from js3-mode.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3-mode" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("js3-mode-pkg.el") (22369 32101 106311
-;;;;;;  704000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; js3-mode-autoloads.el ends here
