10fd31ad766ac9306abf9c11817f00fc09674d0f
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2017-11-21 22:20:41 +0300

    add new packages

---
+++ elpa/flycheck-plantuml-20171017.1811/flycheck-plantuml-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-plantuml" "20171017.1811" "Integrate plantuml with flycheck" '((flycheck "0.24") (emacs "24.4") (plantuml-mode "1.2.2")) :commit "183be89e1dbba0b38237dd198dff600e0790309d" :url "https://github.com/alexmurray/flycheck-plantuml")
