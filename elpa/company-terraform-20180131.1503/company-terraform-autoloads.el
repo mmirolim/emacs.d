ab7e6ad56bcda8a736d59c0c19c6f23193aeffc9
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2018-02-28 12:18:32 +0300

    add terraform

---
+++ elpa/company-terraform-20180131.1503/company-terraform-autoloads.el
@@ -0,0 +1,33 @@
+;;; company-terraform-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "company-terraform" "company-terraform.el"
+;;;;;;  (23176 34757 298582 356000))
+;;; Generated autoloads from company-terraform.el
+
+(autoload 'company-terraform "company-terraform" "\
+Main entry point for a company backend.
+Read `company-mode` function docs for the semantics of this function.
+
+\(fn COMMAND &optional ARG &rest IGNORED)" nil nil)
+
+(autoload 'company-terraform-init "company-terraform" "\
+Add terraform to the company backends.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("company-terraform-data.el" "company-terraform-pkg.el")
+;;;;;;  (23176 34757 286582 333000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; company-terraform-autoloads.el ends here
