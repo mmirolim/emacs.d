184c94bddc83a48fabab59550ef3f0464be81d70
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2018-08-23 01:05:55 +0300

    update

--- elpa/f-20180106.122/f-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("f.el") (23128 32929 969331 944000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; f-autoloads.el ends here
--- elpa/syntax-subword-20160519.1205/syntax-subword-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "syntax-subword" "20160519.1205" "make operations on words more fine-grained" 'nil)
