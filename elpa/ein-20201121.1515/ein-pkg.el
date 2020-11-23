eb07836b146eb96ba95da37e22d53571b33619c6
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2020-11-23 17:12:02 +0500

    update packages

---
+++ elpa/ein-20201121.1515/ein-pkg.el
@@ -0,0 +1,13 @@
+(define-package "ein" "20201121.1515" "Emacs IPython Notebook"
+  '((emacs "25")
+    (websocket "20190620")
+    (anaphora "20180618")
+    (request "20200117")
+    (deferred "0.5")
+    (polymode "20190714")
+    (dash "2.13.0")
+    (with-editor "20200522"))
+  :commit "1b0bf84a2aeb9c10d299050dd29e7c6528b52a0f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/git-commit-20201115.2313/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/mm/.emacs.d/elpa/git-commit-20201115.2313/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20201115.2313" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "97ccce9b4823755fb01a3db01e54cf851cc5c065" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
