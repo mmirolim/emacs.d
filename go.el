80b25fc7f3a0b769e1f6947240ff6ed21a0d994d
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2022-08-01 15:30:00 +0500

    remove old go tools

--- go.el
+++ go.el
@@ -4,21 +4,16 @@
 
 (require 'go-mode)
 (require 'go-guru)
-(require 'go-eldoc)
 (require 'yasnippet)
 (require 'flycheck)
 (require 'go-direx)
 (require 'company)
-(require 'company-go)
 
 ;;; Code:
 ;; TODO should be able to get from ENV but does not work
 (setenv "GOPATH" "/home/mm/go")
 (setq exec-path (append exec-path '("/home/mm/go/bin")))
 
-; Golint
-(add-to-list 'load-path (concat (getenv "GOPATH")
-				"/src/golang.org/x/lint/misc/emacs"))
 
 (use-package lsp-mode
   :ensure t
@@ -48,31 +43,11 @@
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 1))
 
-(require 'golint)
-;; (defun my-go-mode-hook ()
-;; 	;; Use goimports instead of go-fmt
-;; 	(setq gofmt-command "goimports")
-;; 	;; Call gofmt before saving
-;; 	(add-hook 'before-save-hook 'gofmt-before-save)
-;; 	;; Customize compile command to run go build
-;; 	(if (not (string-match "go" compile-command))
-;; 		(set (make-local-variable 'compile-command)
-;; 			;; "go build -v && go test -v"))
-;; 			;; go vet not working
-;; 			"go build -v && go test -v && go vet"))
-;; 	;; Godef jump key binding
-;; 	(local-set-key (kbd "M-.") 'godef-jump))
-
-;; (add-hook 'go-mode-hook 'my-go-mode-hook)
 ; kbd to godoc
 (add-hook 'go-mode-hook '(lambda ()
 	(local-set-key (kbd "C-c C-k") 'godoc)))
 ; godoc autocomplete
 (add-hook 'go-mode-hook 'company-mode)
-(add-hook 'go-mode-hook (lambda ()
-	(set (make-local-variable 'company-backends) '(company-go))
-	(company-mode)))
-(add-hook 'go-mode-hook 'go-eldoc-setup)
 (setq company-tooltip-limit 20)                      ; bigger popup window
 (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
